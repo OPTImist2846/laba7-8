eb756f40508831c41c1a3ea8ee6b1f5771defae9
OPTImist2846 <olessolja@gmail.com>
2025-05-27 17:34:46 +0300

    add adding on start list

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -12,6 +12,25 @@ private:
     std::unique_ptr<Node<T>> head;
     Node<T>* tail; // Raw pointer to the tail, as unique_ptr cannot be held by multiple owners
     int size;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    // Destructor (unique_ptr handles head, remaining nodes are linked via next)
+    ~DoublyLinkedList() = default;
+
+    void pushFront(T val) {
+        std::unique_ptr<Node<T>> newNode = std::make_unique<Node<T>>(val);
+        if (!head) {
+            head = std::move(newNode);
+            tail = head.get();
+        } else {
+            newNode->next = std::move(head);
+            head->prev = newNode.get();
+            head = std::move(newNode);
+        }
+        size++;
+    }
 };
 
 
