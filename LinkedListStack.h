e956ac5a8fcfdad4fa53a177c44b248c6535c14c
OPTImist2846 <olessolja@gmail.com>
2025-05-27 20:39:24 +0300

    add class LinkedListStack pop metod

--- LinkedListStack.h
+++ LinkedListStack.h
@@ -27,5 +27,16 @@ public:
         topNode = std::move(newNode); // Новий вузол стає topNode
         currentSize++;
     }
+
+    // b. Pop: Видаляє і повертає елемент з вершини стеку
+    T pop() {
+        if (isEmpty()) {
+            throw std::runtime_error("Stack underflow: Cannot pop from an empty stack.");
+        }
+        T val = topNode->data;
+        topNode = std::move(topNode->next); // topNode переміщується на наступний вузол
+        currentSize--;
+        return val;
+    }
     };
 #endif //LINKEDLISTSTACK_H
