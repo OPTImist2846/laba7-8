aeabcdf2eea590032023afae5a1942f93273e83e
OPTImist2846 <olessolja@gmail.com>
2025-05-27 20:48:44 +0300

    add LinkedListStack peek method

--- LinkedListStack.h
+++ LinkedListStack.h
@@ -38,5 +38,13 @@ public:
         currentSize--;
         return val;
     }
+
+    // c. Peek: Повертає елемент з вершини стеку без видалення
+    T peek() const {
+        if (isEmpty()) {
+            throw std::runtime_error("Stack is empty: Cannot peek at an empty stack.");
+        }
+        return topNode->data;
+    }
     };
 #endif //LINKEDLISTSTACK_H
