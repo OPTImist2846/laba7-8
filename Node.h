8aadc3824730ea1f601f17e70c281a8b4b09ee9a
OPTImist2846 <olessolja@gmail.com>
2025-05-27 14:43:50 +0300

    add Node unique ptr, raw ptr

--- Node.h
+++ Node.h
@@ -7,7 +7,9 @@ template <typename T>
 struct Node {
     T data;
     std::unique_ptr<Node<T>> next;
+    Node<T>* prev;
 
     Node(T val) : data(val), next(nullptr), prev(nullptr) {}
+    Node(T val, Node<T>* p) : data(val), next(nullptr), prev(p) {}
 };
 
