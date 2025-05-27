b9f9fa2a2aee19efa00160da14120668dd2605a7
OPTImist2846 <olessolja@gmail.com>
2025-05-27 20:39:03 +0300

    add class LinkedListStack push metod

--- LinkedListStack.h
+++ LinkedListStack.h
@@ -18,5 +18,14 @@ public:
     // Деструктор: unique_ptr автоматично звільняє пам'ять всіх вузлів
     ~LinkedListStack() = default;
 
+    // a. Push: Додає елемент на вершину стеку
+    void push(T val) {
+        std::unique_ptr<Node<T>> newNode = std::make_unique<Node<T>>(val);
+        if (topNode) {
+            newNode->next = std::move(topNode); // Новий вузол вказує на поточний topNode
+        }
+        topNode = std::move(newNode); // Новий вузол стає topNode
+        currentSize++;
+    }
     };
 #endif //LINKEDLISTSTACK_H
